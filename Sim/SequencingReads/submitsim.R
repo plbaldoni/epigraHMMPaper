25ca46a3750b70e1700ef65c9146193800633387
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-01-20 08:06:05 +1100

    Fixing directories

--- Sim/SequencingReads/submitsim.R
+++ Sim/SequencingReads/submitsim.R
@@ -1,38 +1,50 @@
 
 iters <- 1:100
 is.tf <- F
-wd <- getwd()
+wd <- '/stornext/SBPM/baldoni.p/epigraHMMPaper/Sim/SequencingReads'
 
-for(tf in is.tf){
-    for(it in iters){
+for (tf in is.tf) {
+    for (it in iters) {
         # Creating code directory
-        cmd = paste0('mkdir autosim',it)
-        cat('Command: ',cmd)
-        system(cmd)
+        dir.create(file.path(wd,paste0('autosim',it)))
         
         # Copying autosim.R to tmp.R
-        cmd = paste0('cp autosim.R tmp.R')
+        cmd = paste('cp',file.path(wd,'autosim.R'),file.path(wd,'tmp.R'))
         cat('Command: ',cmd)
         system(cmd)
         
         # Writing the specification to the top of the file autosim.R
-        cmd = paste0("echo '",paste0('is.tf = ',tf),paste0(';iters = ',it),"' | cat - tmp.R > ",paste0('./autosim',it,".R"))
+        cmd = paste0("echo '",paste0('is.tf = ',tf),paste0(';iters = ',it),"' | cat - ",file.path(wd,'tmp.R')," > ",
+                     file.path(wd,paste0('autosim',it,'.R')))
         cat('Command: ',cmd)
         system(cmd)
         
         # Moving new autosim
-        cmd = paste0('mv autosim',it,'.R ./autosim',it,'/')
+        cmd = paste('mv',file.path(wd,paste0('autosim',it,'.R')),file.path(wd,paste0('autosim',it)))
         cat('Command: ',cmd)
         system(cmd)
         
         # Remove tmp
-        cmd = paste0('rm tmp.R')
+        cmd = paste('rm',file.path(wd,'tmp.R'))
         cat('Command: ',cmd)
         system(cmd)
         
         # Submitting jobs, change it if not using SLURM
-        setwd(paste0('./autosim',it,'/'))
-        cmd = paste0('sbatch -t 12:00:00 --mem=16g R CMD BATCH ./autosim',it,".R")
+        setwd(file.path(wd,paste0('autosim',it)))
+        
+        sink(file = paste0('autosim',it,'.sh'))
+        cat(
+'#!/bin/bash
+ 
+#SBATCH -t 12:00:00
+#SBATCH --mem=8g
+ 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate py36
+R CMD BATCH ',paste0('autosim',it,'.R'))
+        sink()
+        
+        cmd = paste('sbatch',paste0('autosim',it,'.sh'))
         cat('Command: ',cmd)
         system(cmd)
         setwd(wd)
