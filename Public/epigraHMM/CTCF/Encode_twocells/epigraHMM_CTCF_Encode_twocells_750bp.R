853702525ec7812cf26e859e7380007899ac0f2d
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-02-09 03:52:53 +1100

    Adding microbenchmark

--- Public/epigraHMM/CTCF/Encode_twocells/epigraHMM_CTCF_Encode_twocells_750bp.R
+++ Public/epigraHMM/CTCF/Encode_twocells/epigraHMM_CTCF_Encode_twocells_750bp.R
@@ -58,7 +58,7 @@ for (idx in seq_len(length(rdata))) {
   rm(dat)
 }
 
-colData <- colData[chr %in% chromosome,]
+colData <- colData[chr %in% chromosome, ]
 rowRanges <-
   makeGRangesFromDataFrame(
     colData,
@@ -81,61 +81,46 @@ saveerror = function(name, text) {
 }
 
 # epigraHMM
-tryCatch({
-  assign(paste(method, mark, type, 'Time', paste0(bin, 'bp'), sep = '_'),
-         system.time({
-           # Creating object
-           out.epigraHMM <- epigraHMM::epigraHMMDataSetFromMatrix(
-             countData = as.matrix(colData[, paste(condition, replicate, sep = '.'), with = FALSE]),
-             colData = data.frame(condition = condition, replicate = replicate),
-             rowRanges = rowRanges
-           )
-           
-           # Removing unecessary objects
-           rm(colData, rowRanges)
-           
-           # Normalization
-           out.epigraHMM <-
-             epigraHMM::normalizeCounts(out.epigraHMM, control = controlEM(), span = 1)
-           
-           # Initialization
-           out.epigraHMM <-
-             epigraHMM::initializer(object = out.epigraHMM,
-                                    control = epigraHMM::controlEM(maxIterEM = 15))
-           
-           assign(
-             paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_'),
-             epigraHMM::epigraHMM(
-               object = out.epigraHMM,
-               type = 'differential',
-               dist = 'nb',
-               control = epigraHMM::controlEM(
-                 tempDir = normalizePath(diroutput),
-                 fileName = paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_')
-               )
-             )
-           )
-         }))
-}, warning = function(e) {
-  cat('WARNING: ', conditionMessage(e), "\n")
-  saveerror(paste0(paste(
-    method, mark, type, 'Error', paste0(bin, 'bp'), sep = '_'
-  ), '.txt'), e)
-}, error = function(e) {
-  cat('ERROR: ', conditionMessage(e), "\n")
-  saveerror(paste0(paste(
-    method, mark, type, 'Error', paste0(bin, 'bp'), sep = '_'
-  ), '.txt'), e)
-})
-
+cptime <- list()
+cptime[[paste(method, mark, type, 'Time', paste0(bin, 'bp'), sep = '_')]] <-
+  microbenchmark::microbenchmark({
+    # Creating object
+    out.epigraHMM <- epigraHMM::epigraHMMDataSetFromMatrix(
+      countData = as.matrix(colData[, paste(condition, replicate, sep = '.'), with = FALSE]),
+      colData = data.frame(condition = condition, replicate = replicate),
+      rowRanges = rowRanges
+    )
+    
+    # Removing unecessary objects
+    rm(colData, rowRanges)
+    
+    # Normalization
+    out.epigraHMM <-
+      epigraHMM::normalizeCounts(out.epigraHMM, control = controlEM(), span = 1)
+    
+    # Initialization
+    out.epigraHMM <-
+      epigraHMM::initializer(object = out.epigraHMM,
+                             control = epigraHMM::controlEM(maxIterEM = 15))
+    
+    assign(
+      paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_'),
+      epigraHMM::epigraHMM(
+        object = out.epigraHMM,
+        type = 'differential',
+        dist = 'nb',
+        control = epigraHMM::controlEM(
+          tempDir = normalizePath(diroutput),
+          fileName = paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_')
+        )
+      )
+    )
+  }, times = 1)
 
 ### Saving computing time
-do.call(save, list(
-  paste(method, mark, type, 'Time', paste0(bin, 'bp'), sep = '_'),
-  file = paste0(diroutput, paste(
-    method, mark, type, 'Time', paste0(bin, 'bp', '.RData'), sep = '_'
-  ))
-))
+save(cptime,file = paste0(diroutput, paste(
+  method, mark, type, 'Time', paste0(bin, 'bp', '.RData'), sep = '_'
+)))
 
 ### Saving output
 do.call(save, list(
@@ -147,10 +132,17 @@ do.call(save, list(
 
 ### Saving bed file
 
-tobed <- get(paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_'))
-tobed <- callPeaks(object = tobed,
-                   hdf5 = file.path(diroutput,paste(method, mark, type, 'Output', paste0(bin, 'bp.h5'), sep = '_')),
-                   method = 'viterbi',
-                   saveToFile = FALSE)
+tobed <-
+  get(paste(method, mark, type, 'Output', paste0(bin, 'bp'), sep = '_'))
+tobed <- callPeaks(
+  object = tobed,
+  hdf5 = file.path(diroutput, paste(
+    method, mark, type, 'Output', paste0(bin, 'bp.h5'), sep = '_'
+  )),
+  method = 'viterbi',
+  saveToFile = FALSE
+)
 
-rtracklayer::export.bed(object = tobed,con = file.path(diroutput,paste(method, mark, type, 'Bed', paste0(bin, 'bp.bed'), sep = '_')))
+rtracklayer::export.bed(object = tobed, con = file.path(diroutput, paste(
+  method, mark, type, 'Bed', paste0(bin, 'bp.bed'), sep = '_'
+)))
